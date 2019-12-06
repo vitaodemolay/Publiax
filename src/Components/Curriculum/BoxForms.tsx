0f71b9247188781029ac6642738bf8f4f79ebe65
Thiago Ferrari <thiago.ferrari@bpfidelidade.com.br>
2019-12-06 19:29:33 -0300

    inclusão links

--- src/Components/Curriculum/BoxForms.tsx
+++ src/Components/Curriculum/BoxForms.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import DadosPessoaisComponent from './Forms/DadosPessoais';
-import { Switch, Route, Redirect } from 'react-router';
 import DadosEnderecoComponent from './Forms/DadosEndereco';
+import DadosProfissionaisComponent from './Forms/DadosProfissionais';
+import DadosEscolaridadeComponent from './Forms/DadosEscolaridade';
+import InformaticaComponent from './Forms/Informatica';
 
+import { Switch, Route, Redirect } from 'react-router';
 
 export default class BoxFormsComponent extends React.Component {
     render() {
@@ -12,6 +15,10 @@ export default class BoxFormsComponent extends React.Component {
                 <Switch>
                     <Route path="/curriculum/Dadospessoais" component={DadosPessoaisComponent}/>
                     <Route path="/curriculum/Dadosendereco" component={DadosEnderecoComponent}/>
+                    <Route path="/curriculum/DadosProfissionais" component={DadosProfissionaisComponent}/>
+                    <Route path="/curriculum/DadosEscolaridade" component={DadosEscolaridadeComponent}/>
+                    <Route path="/curriculum/Informatica" component={InformaticaComponent}/>
+
                     <Redirect to="/curriculum/Dadospessoais" />
                 </Switch>
             </Container>
--- src/Components/Curriculum/Sidebar.tsx
+++ src/Components/Curriculum/Sidebar.tsx
@@ -10,25 +10,25 @@ export default class SidebarComponent extends React.Component {
                     <h1 className="">Cúrriculo</h1>
                     <ListGroupItem tag="a" href="#/curriculum/Dadospessoais"><span className="lnr lnr-user"></span>Dados Pessoais</ListGroupItem>
                     <ListGroupItem tag="a" href="#/curriculum/Dadosendereco"><span className="lnr lnr-user"></span>Dados de Endereço</ListGroupItem>
-                    <ListGroupItem tag="a" href="" ><span className="lnr lnr-briefcase"></span>Dados Profissionais</ListGroupItem>
-                    <ListGroupItem tag="a" href="" ><span className="lnr lnr-graduation-hat"></span>Escolaridade</ListGroupItem>
-                    <ListGroupItem tag="a" href="" ><span className="lnr lnr-screen"></span>Informática</ListGroupItem>
-                    <ListGroupItem tag="a" href="" ><span className="lnr lnr-bubble"></span>Línguas Estrangeiras</ListGroupItem>
-                    <ListGroupItem tag="a" href="" ><span className="lnr lnr-question-circle"></span>Outras Informações</ListGroupItem>
-                    <ListGroupItem tag="a" href="" ><span className="lnr lnr-license"></span>Carta de Apresentação</ListGroupItem>
+                    <ListGroupItem tag="a" href="#/curriculum/DadoProfissionais" ><span className="lnr lnr-briefcase"></span>Dados Profissionais</ListGroupItem>
+                    <ListGroupItem tag="a" href="#/curriculum/DadosEscolaridade" ><span className="lnr lnr-graduation-hat"></span>Escolaridade</ListGroupItem>
+                    <ListGroupItem tag="a" href="#/curriculum/Informatica" ><span className="lnr lnr-screen"></span>Informática</ListGroupItem>
+                    <ListGroupItem tag="a" href="#/curriculum/Linguas" ><span className="lnr lnr-bubble"></span>Línguas Estrangeiras</ListGroupItem>
+                    <ListGroupItem tag="a" href="#/curriculum/Informacoes" ><span className="lnr lnr-question-circle"></span>Outras Informações</ListGroupItem>
+                    <ListGroupItem tag="a" href="#/curriculum/Apresentacao" ><span className="lnr lnr-license"></span>Carta de Apresentação</ListGroupItem>
                 </ListGroup>
 
                 <ListGroup>
-                    <ListGroupItem tag="a" href="" ><span className="lnr lnr-eye"></span>Visualizar</ListGroupItem>
-                    <ListGroupItem tag="a" href="" ><span className="lnr lnr-printer"></span>Imprimir</ListGroupItem>
+                    <ListGroupItem tag="a" href="#/curriculum/Apresentacao" ><span className="lnr lnr-eye"></span>Visualizar</ListGroupItem>
+                    <ListGroupItem tag="a" href="#/curriculum/Apresentacao" ><span className="lnr lnr-printer"></span>Imprimir</ListGroupItem>
                 </ListGroup>
 
                 <ListGroup>
                     <h1 className="">Minha Conta</h1>
-                    <ListGroupItem tag="a" href="" ><span className="lnr lnr-users"></span>Curriculo</ListGroupItem>
-                    <ListGroupItem tag="a" href="" ><span className="lnr lnr-alarm"></span>Avisos</ListGroupItem>
-                    <ListGroupItem tag="a" href="" ><span className="lnr lnr-exit-up"></span>Participações</ListGroupItem>
-                    <ListGroupItem tag="a" href="" ><span className="lnr lnr-cog"></span>Configurações</ListGroupItem>
+                    <ListGroupItem tag="a" href="#/curriculum/Apresentacao" ><span className="lnr lnr-users"></span>Curriculo</ListGroupItem>
+                    <ListGroupItem tag="a" href="#/curriculum/Apresentacao" ><span className="lnr lnr-alarm"></span>Avisos</ListGroupItem>
+                    <ListGroupItem tag="a" href="#/curriculum/Apresentacao" ><span className="lnr lnr-exit-up"></span>Participações</ListGroupItem>
+                    <ListGroupItem tag="a" href="#/curriculum/Apresentacao" ><span className="lnr lnr-cog"></span>Configurações</ListGroupItem>
                 </ListGroup>
             </Container>
 
