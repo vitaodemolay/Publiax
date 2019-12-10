1b983f25415581b0a273cbd9e062ed7fa05b66e0
Thiago Ferrari <thiago.ferrari@bpfidelidade.com.br>
2019-12-10 19:05:54 -0300

    inserção paginas

--- src/Components/Curriculum/BoxForms.tsx
+++ src/Components/Curriculum/BoxForms.tsx
@@ -3,8 +3,20 @@ import { Container } from 'reactstrap';
 import DadosPessoaisComponent from './Forms/DadosPessoais';
 import DadosEnderecoComponent from './Forms/DadosEndereco';
 import DadosProfissionaisComponent from './Forms/DadosProfissionais';
-import DadosEscolaridadeComponent from './Forms/DadosEscolaridade';
+import EscolaridadeComponent from './Forms/Escolaridade';
 import InformaticaComponent from './Forms/Informatica';
+import LinguasEstrangeirasComponent from './Forms/LinguasEstrangeiras';
+import OutrasInformacoesComponent from './Forms/OutrasInformacoes';
+import CartaApresentacaoComponent from './Forms/CartaApresentacao';
+
+import VisualizarComponent from './Forms/Visualizar';
+import ImprimirComponent from './Forms/Imprimir';
+
+import CurriculoComponent from './Forms/Curriculo';
+import AvisosComponent from './Forms/Avisos';
+import ParticipacoesComponent from './Forms/Participacoes';
+import ConfiguracoesComponent from './Forms/Configuracoes';
+
 
 import { Switch, Route, Redirect } from 'react-router';
 
@@ -16,8 +28,19 @@ export default class BoxFormsComponent extends React.Component {
                     <Route path="/curriculum/Dadospessoais" component={DadosPessoaisComponent}/>
                     <Route path="/curriculum/Dadosendereco" component={DadosEnderecoComponent}/>
                     <Route path="/curriculum/DadosProfissionais" component={DadosProfissionaisComponent}/>
-                    <Route path="/curriculum/DadosEscolaridade" component={DadosEscolaridadeComponent}/>
+                    <Route path="/curriculum/Escolaridade" component={EscolaridadeComponent}/>
                     <Route path="/curriculum/Informatica" component={InformaticaComponent}/>
+                    <Route path="/curriculum/LinguasEstrangeiras" component={LinguasEstrangeirasComponent}/>
+                    <Route path="/curriculum/OutrasInformacoes" component={OutrasInformacoesComponent}/>
+                    <Route path="/curriculum/CartaApresentacao" component={CartaApresentacaoComponent}/>
+
+                    <Route path="/curriculum/Visualizar" component={VisualizarComponent}/>
+                    <Route path="/curriculum/Imprimir" component={ImprimirComponent}/>
+
+                    <Route path="/curriculum/CartaApresentacao" component={CartaApresentacaoComponent}/>
+                    <Route path="/curriculum/Avisos" component={AvisosComponent}/>
+                    <Route path="/curriculum/Participacoes" component={ParticipacoesComponent}/>
+                    <Route path="/curriculum/Configuracoes" component={ConfiguracoesComponent}/>
 
                     <Redirect to="/curriculum/Dadospessoais" />
                 </Switch>
