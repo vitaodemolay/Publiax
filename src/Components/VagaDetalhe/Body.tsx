7d23ceb4e51b0e708c6d70c549cf40d06721bea9
Thiago Ferrari <thiago.ferrari@bpfidelidade.com.br>
2019-12-18 19:21:37 -0300

    ajustes detalhes vagas

--- src/Components/VagaDetalhe/Body.tsx
+++ src/Components/VagaDetalhe/Body.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { IVaga } from '../../Service/Interfaces/IVaga';
-import { Container, Row, Col, Card, CardBody, CardHeader, CardFooter } from 'reactstrap';
+import { Container, Col, Card, CardBody, CardHeader, CardFooter } from 'reactstrap';
 import Dynamic from '../Dynamic/DynamicComponent';
 import { VagaTools } from '../../Models/VagaTools';
 
@@ -14,23 +14,21 @@ export default class VagaDetalheBody extends React.Component<Props>{
 
         const { vaga } = this.props;
         return (
-            <Container>
-                <Row>
-                    <Card>
-                        <CardHeader className="text-center" >
-                            <h3>{vaga.titulo}</h3>
-                        </CardHeader>
-                        <CardBody>
-                            <Dynamic html={vaga.descricao} />
-                        </CardBody>
-                        <CardFooter>
-                            <p>
-                                <strong>Publicado Em: </strong>
-                                <span>{VagaTools.getPublicacaoFormat(vaga.publicacao)}</span>
-                            </p>
-                        </CardFooter>
-                    </Card>
-                </Row>
+            <Container className="content-vagas">
+                <Card>
+                    <CardHeader className="text-center" >
+                        <h3>{vaga.titulo}</h3>
+                    </CardHeader>
+                    <CardBody>
+                        <Dynamic html={vaga.descricao} />
+                    </CardBody>
+                    <CardFooter>
+                        <p>
+                            <strong>Publicado Em: </strong>
+                            <span>{VagaTools.getPublicacaoFormat(vaga.publicacao)}</span>
+                        </p>
+                    </CardFooter>
+                </Card>
             </Container>
         );
     }
