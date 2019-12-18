3d35869baa2830120d54f4c46779bb819869b246
Thiago Ferrari <thiago.ferrari@bpfidelidade.com.br>
2019-12-18 16:16:11 -0300

    retirada de html comentado

--- src/Components/Curriculum/Forms/Visualizar.tsx
+++ src/Components/Curriculum/Forms/Visualizar.tsx
@@ -9,57 +9,7 @@ export default class VisualizarComponent extends React.Component {
                 <hr />
                 <Container className="signup-content">
 
-                    <FormGroup>
-                        <Label>Nome Completo</Label>
-                        <Input type="text" className="form-control" placeholder="Nome Completo" />
-                    </FormGroup>
-                    
-                    {/* 
-                    <h2>Endereços</h2>
-                    <hr />
-                    <div className="form-group">
-                        <label>Endereço</label>
-                        <input type="email" className="form-control" placeholder="Endereço" />
-                    </div>
-                    <div className="form-group">
-                        <label>CEP</label>
-                        <input type="email" className="form-control" placeholder="CEP" />
-                    </div>
-                    <div className="row">
-                        <div className="form-group col-md-4">
-                            <label>Numero</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="form-group col-md-8">
-                            <label>Complemento</label>
-                            <input type="text" className="form-control" />
-
-                        </div>
-
-                    </div>
-                    <div className="form-group">
-                        <label>Bairro</label>
-                        <input type="email" className="form-control" />
-                    </div>
-                    <div className="row">
-                        <div className="form-group col-md-4">
-                            <label>Cidade</label>
-                            <select id="inputState" className="form-control">
-                                <option>Escolha...</option>
-                                <option>...</option>
-                            </select>
-                        </div>
-                        <div className="form-group col-md-8">
-                            <label>Estado</label>
-                            <select id="inputState" className="form-control">
-                                <option>Escolha...</option>
-                                <option>...</option>
-                            </select>
-                        </div>
-
-                    </div> */}
-
-                    <Button className="btn btn-primary">SALVAR</Button>
+                 
                 </Container>
             </Form>
         );
