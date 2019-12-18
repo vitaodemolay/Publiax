0f13bf82625be2e8c09b5d60e8ef6a0736eb688f
Thiago Ferrari <thiago.ferrari@bpfidelidade.com.br>
2019-12-18 19:31:10 -0300

    estilização página se logar

--- src/Components/PrivateAccess/Body.tsx
+++ src/Components/PrivateAccess/Body.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Container, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 
 export default class BodyComponent extends React.Component {
     render() {
         return (
-            <Container>
-                <Row>
+            <Container className="box-logar">
                     <p>Para o acesso a este conteudo é necessário que você faça Login!</p>
                     <p>Clique no botão baixo para realizar o login e tenha acesso total ao portal</p>
-                </Row>
-                <Row>
-                    <Link to="/login">Para Logar Clique Aqui</Link>
-                </Row>
+                    <Link to="/login" className="btn-logar">Para Logar Clique Aqui</Link>
             </Container>
         );
     }
