d54ff79e3b1171e4646ea3851d0bfeca29044b37
Thiago Ferrari <thiago.ferrari@bpfidelidade.com.br>
2020-01-31 19:53:39 -0300

    css

--- src/Components/QuemSomos/Vitrine.tsx
+++ src/Components/QuemSomos/Vitrine.tsx
@@ -9,14 +9,14 @@ export default class VitrineComponent extends React.Component {
             <Container>
                 <Row>
                     <Col lg="6">
-                        <Card>
-                            <CardHeader>
-                                <h3>Mais que uma Agência, uma Referência</h3>
-                            </CardHeader>
-                            <CardBody className="winnDescription">
-                                <p>A Winn Promoções e Eventos é uma agência formada por profissionais com vasta experiência no mercado de Marketing promocional e eventos, com forte presença no mercado Imobiliário e corporativo. Mais que uma agência de promoções e eventos, somos referência em todos os serviços oferecidos, tendo sempre como marca a entrega rápida, comprometimento e a eficiência em tudo que faz. Desta maneira você encontra em nossa agência, todas as opções que farão sua empresa se destacar cada vez mais no mercado.</p>
-                            </CardBody>
-                        </Card>
+
+                        <CardHeader>
+                            <h3>Mais que uma Agência, uma Referência</h3>
+                        </CardHeader>
+                        <CardBody className="winnDescription">
+                            <p>A Winn Promoções e Eventos é uma agência formada por profissionais com vasta experiência no mercado de Marketing promocional e eventos, com forte presença no mercado Imobiliário e corporativo. Mais que uma agência de promoções e eventos, somos referência em todos os serviços oferecidos, tendo sempre como marca a entrega rápida, comprometimento e a eficiência em tudo que faz. Desta maneira você encontra em nossa agência, todas as opções que farão sua empresa se destacar cada vez mais no mercado.</p>
+                        </CardBody>
+
                     </Col>
                     <Col lg="6">
                         <img src={imgWinn} className="img-responsive" />
