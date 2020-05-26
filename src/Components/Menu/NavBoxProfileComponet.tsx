f42de2cb037aec5435f49caa53c1444c88dd4c50
Vitor Custodio <vitor@MacBook13Home.local>
2020-05-25 21:28:42 -0300

    Componente de profile adicionado login

--- src/Components/Menu/NavBoxProfileComponet.tsx
+++ src/Components/Menu/NavBoxProfileComponet.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Inject, Connection } from 'exredux';
 import { appModels } from "../../AppModels";
-import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavItem, Spinner } from "reactstrap";
+import { DadosPessoaisModel } from "../../Models/DadosPessoaisModel";
 
 class Props {
-
+    @Inject dadosPessoais?: DadosPessoaisModel;
 }
 
 @Connection({
@@ -13,23 +14,49 @@ class Props {
 })
 
 class NavBoxProfile extends React.Component<Props> {
-    render(){
-        return(
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Usuário <span className="lnr lnr-user"></span>
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Notificações
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem href="#/logout">
-                  Sair
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-        )
+    componentDidMount() {
+        this.loadFields();
+    }
+
+    loadFields() {
+        const { dadosPessoais } = this.props;
+        dadosPessoais.getUserDataOnSource();
+    }
+
+    render() {
+        const { dadosPessoais } = this.props;
+        if (dadosPessoais.isCompleted)
+            return (
+                <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                        <span className="lnr lnr-user"></span>
+                        <span>{dadosPessoais.input.login}</span>
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                        <DropdownItem>
+                            Notificações
+                        </DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem href="#/logout">
+                            Sair
+                        </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+            )
+
+        if (dadosPessoais.isLoading)
+            return (
+                <NavItem>
+                    <Spinner animation="border" variant="secondary" />
+                </NavItem>
+            )
+
+        else
+            return (
+                <NavItem>
+                    <span>Erro!</span>
+                </NavItem>
+            )
     }
 }
 
