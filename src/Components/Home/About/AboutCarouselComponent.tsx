5fa6be490062491dfde071d265d9e007467de8c8
Vitor Custodio <vitor@MacBook13Home.local>
2020-06-04 09:05:38 -0300

    carousel funcionando

--- src/Components/Home/About/AboutCarouselComponent.tsx
+++ src/Components/Home/About/AboutCarouselComponent.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Container, CarouselItem, Carousel, CarouselControl, CarouselCaption, CarouselIndicators } from "reactstrap";
+import React from "react";
+import { Container, UncontrolledCarousel } from "reactstrap";
 import { IAboutBoxProps } from "../../../Service/Interfaces/IAboutBoxProps";
 import AboutCarouselItem from "./AboutCarouselItemComponent";
 
@@ -7,16 +7,40 @@ interface Props {
     aboutBoxItens: IAboutBoxProps[]
 }
 
-
+const carouselBackGrd = require('../../../img/backgroud_carousel.png');
 export default class AboutCarousel extends React.Component<Props>{
 
-    render() {
+    private generateCarouselItens(): any[] {
         const { aboutBoxItens } = this.props;
+        const numberItens = Math.ceil(aboutBoxItens.length / 3);
+        const result: any[] = [];
+        const increment = 3;
+        let startIndex = 0;
+        let endIndex = increment;
+        for (let index = 0; index < numberItens; index++) {
+            const sliceAboutBoxItens = aboutBoxItens.slice(startIndex, endIndex);
+            startIndex = endIndex;
+            endIndex += increment;
+            result.push({
+                src: carouselBackGrd,
+                altText: '',
+                header: <AboutCarouselItem aboutBoxItens={sliceAboutBoxItens} />,
+                caption: <React.Fragment />,
+                key: index
+            })
+        }
+        return result;
+    }
+
+
+    render() {
+        const carouselItens = this.generateCarouselItens();
         return (
             <Container>
-                <AboutCarouselItem aboutBoxItens={aboutBoxItens} />
+                <UncontrolledCarousel items={carouselItens}  />
             </Container>
         );
     }
 
 }
+
